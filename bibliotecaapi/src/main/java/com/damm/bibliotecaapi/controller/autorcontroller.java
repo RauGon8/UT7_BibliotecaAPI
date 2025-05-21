3c30c41ab6e1180ba1ebc9d359dc5c4cf3d7582a
RauGon8 <raulgonzalezherrera8@gmail.com>
2025-05-21 23:01:32 +0100

    autor controller

--- bibliotecaapi/src/main/java/com/damm/bibliotecaapi/controller/autorcontroller.java
+++ bibliotecaapi/src/main/java/com/damm/bibliotecaapi/controller/autorcontroller.java
@@ -1,5 +1,35 @@
 package com.damm.bibliotecaapi.controller;
 
-public class autorcontroller {
-    
+import com.damm.bibliotecaapi.model.autor;
+import com.damm.bibliotecaapi.service.AutorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/autores")
+public class autorController {
+    private final AutorService autorService;
+
+    public autorController(AutorService autorService) {
+        this.autorService = autorService;
+    }
+
+    @GetMapping
+    public List<autor> getAllAutores() {
+        return autorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<autor> getAutorById(@PathVariable Long id) {
+        return autorService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public autor createAutor(@RequestBody autor autor) {
+        return autorService.save(autor);
+    }
 }
