7ebf92142e36e3acb236a4c788657471644e3bcb
RauGon8 <raulgonzalezherrera8@gmail.com>
2025-05-21 23:24:15 +0100

    link al repositorio

--- bibliotecaapi/src/main/java/com/damm/bibliotecaapi/BibliotecaapiApplication.java
+++ bibliotecaapi/src/main/java/com/damm/bibliotecaapi/BibliotecaapiApplication.java
@@ -1,5 +1,7 @@
 package com.damm.bibliotecaapi;
 
+//https://github.com/RauGon8/UT7_BibliotecaAPI.git
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
