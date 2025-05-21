b77be82810f80cd7518cd962268660a04ad2dc07
RauGon8 <raulgonzalezherrera8@gmail.com>
2025-05-21 22:08:22 +0100

    repositorio de autor

--- bibliotecaapi/src/main/java/com/damm/bibliotecaapi/repository/AutorRepository.java
+++ bibliotecaapi/src/main/java/com/damm/bibliotecaapi/repository/AutorRepository.java
@@ -1,5 +1,7 @@
 package com.damm.bibliotecaapi.repository;
 
-public class AutorRepository {
-    
+import com.damm.bibliotecaapi.model.autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<autor, Long> {
 }
