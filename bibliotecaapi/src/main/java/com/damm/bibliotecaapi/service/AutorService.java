188fe6f16957236cdbbf29378daf16ab03b24e5b
RauGon8 <raulgonzalezherrera8@gmail.com>
2025-05-21 22:25:36 +0100

    Autor service.

--- bibliotecaapi/src/main/java/com/damm/bibliotecaapi/service/AutorService.java
+++ bibliotecaapi/src/main/java/com/damm/bibliotecaapi/service/AutorService.java
@@ -1,5 +1,29 @@
 package com.damm.bibliotecaapi.service;
 
+import com.damm.bibliotecaapi.model.autor;
+import com.damm.bibliotecaapi.repository.AutorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AutorService {
-    
+    private final AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+    public List<autor> findAll() {
+        return autorRepository.findAll();
+    }
+
+    public Optional<autor> findById(Long id) {
+        return autorRepository.findById(id);
+    }
+
+    public autor save(autor autor) {
+        return autorRepository.save(autor);
+    }
 }
